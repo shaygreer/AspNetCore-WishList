73ae284f924e0771bd9ba06a72a5982342c5869b
shaygreer <71538377+shaygreer@users.noreply.github.com>
2021-09-25 21:53:27 -0600

    AllGreen

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Item> List = _context.Items.ToList();
+            return View("Index", List);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var Item = _context.Items.FirstOrDefault(Item => Item.Id == id);
+            _context.Items.Remove(Item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
